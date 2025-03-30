5b9d2212deca825b57dcf05bf6f710255363da54
glykon <jcgwinnett@pm.me>
2025-03-30 22:11:48 +0100

    feat: front end serves on /measure/ and caches chart data 1 hour

--- pages/measure.js
+++ pages/measure.js
@@ -1,41 +1,50 @@
 import Head from 'next/head';
-import { VegaLite } from 'react-vega';
 import React from 'react';
 import { getAllChartJSON } from '../lib/chart_data';
-import VegaEmbed from 'react-vega/lib/VegaEmbed';
+import dynamic from 'next/dynamic';
+
+// Import VegaEmbed with no SSR to avoid hydration issues
+const VegaEmbed = dynamic(
+  () => import('react-vega/lib/VegaEmbed'),
+  { ssr: false }
+);
 
 function parse(jsonString) {
-  return JSON.parse(jsonString)
+  return JSON.parse(jsonString);
 }
 
 export default function FirstPost(props) {
-    return (
+  return (
     <div className="container">
-    <Head>
-      <title>Measurements</title>
-      <link rel="icon" href="icons/tape.ico" />
-    </Head>
+      <Head>
+        <title>Measurements</title>
+        <link rel="icon" href="/measure/icons/tape.ico" />
+      </Head>
       <main>
         <h1>MEASUREMENTS</h1>
 
         <div id="vis">
         </div>
 
-      {props.data.map((chartJson) => (
-        <div> 
-          <VegaEmbed spec={parse(chartJson)}/>
-        </div>)
-      )} 
+        {props.data.map((chartJson, index) => (
+          <div key={index}> 
+            <VegaEmbed 
+              spec={parse(chartJson)}
+              options={{
+                loader: {
+                  baseURL: '/measure/'
+                },
+                renderer: 'svg' // Try SVG renderer instead of canvas
+              }}
+            />
+          </div>
+        ))} 
       </main>
     </div>
-    )
-  }
+  );
+}
 
-  export async function getServerSideProps() {
-    // Fetch data from external API
-    const data = await getAllChartJSON()
-    // Pass data to the page via props
-    return { props: { data } }
-  }
-
-  
+export async function getServerSideProps() {
+  const data = await getAllChartJSON();
+  return { props: { data } };
+}
