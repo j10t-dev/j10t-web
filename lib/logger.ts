a9da1a11eaad3ea4249092f0e9e4e3e60ee7fb4b
glykon <jcgwinnett@pm.me>
2025-07-02 00:49:52 +0100

    fix: log context

--- lib/logger.ts
+++ lib/logger.ts
@@ -6,19 +6,31 @@ export function logInfo(
   message: string,
   context?: Record<string, unknown>,
 ): void {
-  logger.info(message, context);
+  if (context) {
+    logger.info`${message} ${context}`;
+  } else {
+    logger.info(message);
+  }
 }
 
 export function logWarn(
   message: string,
   context?: Record<string, unknown>,
 ): void {
-  logger.warn(message, context);
+  if (context) {
+    logger.warn`${message} ${context}`;
+  } else {
+    logger.warn(message);
+  }
 }
 
 export function logError(
   message: string,
   context?: Record<string, unknown>,
 ): void {
-  logger.error(message, context);
+  if (context) {
+    logger.error`${message} ${context}`;
+  } else {
+    logger.error(message);
+  }
 }
